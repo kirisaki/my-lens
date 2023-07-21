31251d744f9ab78c6bbd78afc50860f2f5204f8c
Akihito Kirisaki <endo.ryusuke@plus-zero.co.jp>
2023-07-21 20:26:27 +0900

    feat: define getter

--- src/Control/MyLens.hs
+++ src/Control/MyLens.hs
@@ -11,5 +11,13 @@ data Bar = Bar
   , _data  :: String
   } deriving Show
 
+type LensGetter s a = s -> a
+
+xGetter :: LensGetter Foo Int
+xGetter = _x
+
+(^.) :: s -> LensGetter s a -> a
+s ^. f = f s
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
