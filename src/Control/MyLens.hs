6ee5a2bdf49dd9435469f30b0054715de70869aa
Akihito Kirisaki <endo.ryusuke@plus-zero.co.jp>
2023-07-21 20:22:12 +0900

    feat: define data type

--- src/Control/MyLens.hs
+++ src/Control/MyLens.hs
@@ -1,4 +1,15 @@
-module Control.MyLens (someFunc) where
+module Control.MyLens where
+
+data Foo = Foo
+  { _x   :: Int
+  , _y   :: Int
+  , _bar :: Bar
+  } deriving Show
+
+data Bar = Bar
+  { _label :: String
+  , _data  :: String
+  } deriving Show
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
